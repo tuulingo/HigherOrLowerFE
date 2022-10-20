fa8822839469dcd087fd2eafd8738a4e0dcc2f9d
tuulingo <oliver.tuuling@tptlive.ee>
2022-10-20 16:53:00 +0300

    Add movie service and apiurl environment

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Movie } from './movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  private apiServerUrl = environment.APIBASEURL
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(`${this.apiServerUrl}/movie/all`)
+  }
+
+  public getMovie(movieId: BigInt): Observable<Movie> {
+    return this.http.get<Movie>(`${this.apiServerUrl}/movie/find/${movieId}`)
+  }
 }
