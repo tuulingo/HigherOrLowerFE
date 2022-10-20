588bec245029ac98f07a64c665d99d361b0e786d
tuulingo <oliver.tuuling@tptlive.ee>
2022-10-20 16:27:40 +0300

    Add movie model

---
+++ src/app/movie.ts
@@ -0,0 +1,13 @@
+export interface Movie {
+    ID: BigInt;
+    ORIGINAL_TITLE: string;
+    OVERVIEW: string;
+    POPULARITY: Number;
+    RELEASE_DATE: Date;
+    REVENUE: BigInt;
+    RUNTIME: number;
+    TAGLINE: string;
+    TITLE: string;
+    VOTE_AVERAGE: number;
+    VOTE_COUNT: number;
+}
