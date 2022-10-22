ffe531c7509983d13a0da016728fc5f296872a0c
tuulingo <oliver.tuuling@tptlive.ee>
2022-10-22 15:19:11 +0300

    Playable higer or lower with score counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,25 @@ import { MovieService } from './movie.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-// samat moviet ei saa võrrleda
 export class AppComponent {
   title = 'HigherOrLowerFE';
   public movies: Movie[];
-  public initialMovie: Movie;
   public revealedMovie: Movie;
   public hiddenMovie: Movie;
   public isHardMode: Boolean;
   public totalMoviesCount: number;
-  public categoryPlayed: string = "";
+  public categoryPlayed: string = '';
+  public score: number = 0;
+  public categories: any[] = [
+    'vote_AVERAGE',
+    'popularity',
+    'runtime',
+    'revenue',
+  ];
 
-  constructor(private movieService: MovieService){}
+  constructor(private movieService: MovieService) {}
 
   ngOnInit() {
     this.getMovies();
@@ -28,14 +33,17 @@ export class AppComponent {
   public getMovie(movieId: number): void {
     this.movieService.getMovie(movieId).subscribe(
       (response: Movie) => {
-        this.initialMovie = response;
-        console.log(response)
+        if (this.revealedMovie === null || this.revealedMovie === undefined) {
+          this.revealedMovie = response;
+        } else {
+          this.hiddenMovie = response;
+        }
       },
       (error: HttpErrorResponse) => {
-        console.log(error.message)
+        console.log(error.message);
         alert(error.message);
       }
-    )
+    );
   }
 
   public getMovies() {
@@ -43,24 +51,121 @@ export class AppComponent {
       (response: Movie[]) => {
         this.movies = response;
         this.totalMoviesCount = this.movies.length;
-        console.log(this.totalMoviesCount)
+        console.log(this.totalMoviesCount);
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
       }
-    )
+    );
   }
 
-  public onClickCategory(category: string): void{
-    this.getMovie(Math.round(Math.random() * this.totalMoviesCount))
-    this.categoryPlayed = category
-      var homeScreen = document.getElementById("home-screen");
-      if (this.categoryPlayed !== "" || this.categoryPlayed !== null) {
-        homeScreen.style.display = "none";
-      } else {
-        homeScreen.style.display = "block";
-      }
-    console.log(this.categoryPlayed)
+  public onClickCategory(category: string): void {
+    this.getMovie(Math.round(Math.random() * this.totalMoviesCount));
+    this.getMovie(Math.round(Math.random() * this.totalMoviesCount));
+    // while (this.revealedMovie === this.hiddenMovie){
+    //   this.getMovie(Math.round(Math.random() * this.totalMoviesCount))
+    // }
+    this.categoryPlayed = category;
+    var homeScreen = document.getElementById('home-screen');
+    if (this.categoryPlayed !== '' || this.categoryPlayed !== null) {
+      homeScreen.style.display = 'none';
+    } else {
+      homeScreen.style.display = 'block';
+    }
   }
 
+  // TODO: mõtle siia mingi parem lahendus, palun
+  // for (let i = 0; i < this.categories.length; i++) {
+  //   if(this.categories[i] === this.categoryPlayed) {
+  //     this.revealedMovie.categoryPlayed
+  //   }
+  // } ["vote_AVERAGE", "popularity", "runtime", "revenue"];
+
+  public onHigherClicked(): void {
+    switch (this.categoryPlayed) {
+      case 'vote_AVERAGE':
+        if (this.revealedMovie.vote_AVERAGE < this.hiddenMovie.vote_AVERAGE) {
+          this.correctAnswerClicked();
+        } else {
+          alert(
+            `Game ended with a score of: ${this.score}\n${this.hiddenMovie.original_TITLE} had vote average of: ${this.hiddenMovie.vote_AVERAGE}`
+          );
+        }
+        break;
+      case 'popularity':
+        if (this.revealedMovie.popularity < this.hiddenMovie.popularity) {
+          this.correctAnswerClicked();
+        } else {
+          alert(
+            `Game ended with a score of: ${this.score}\n${this.hiddenMovie.original_TITLE} had vote average of: ${this.hiddenMovie.popularity}`
+          );
+        }
+        break;
+      case 'runtime':
+        if (this.revealedMovie.runtime < this.hiddenMovie.runtime) {
+          this.correctAnswerClicked();
+        } else {
+          alert(
+            `Game ended with a score of: ${this.score}\n${this.hiddenMovie.original_TITLE} had vote average of: ${this.hiddenMovie.runtime}`
+          );
+        }
+        break;
+      case 'revenue':
+        if (this.revealedMovie.revenue < this.hiddenMovie.revenue) {
+          this.correctAnswerClicked();
+        } else {
+          alert(
+            `Game ended with a score of: ${this.score}\n${this.hiddenMovie.original_TITLE} had vote average of: ${this.hiddenMovie.revenue}`
+          );
+        }
+        break;
+    }
+  }
+
+  public onLowerClicked(): void {
+    switch (this.categoryPlayed) {
+      case 'vote_AVERAGE':
+        if (this.revealedMovie.vote_AVERAGE > this.hiddenMovie.vote_AVERAGE) {
+          this.correctAnswerClicked();
+        } else {
+          alert(
+            `Game ended with a score of: ${this.score}\n${this.hiddenMovie.original_TITLE} had vote average of: ${this.hiddenMovie.vote_AVERAGE}`
+          );
+        }
+        break;
+      case 'popularity':
+        if (this.revealedMovie.popularity > this.hiddenMovie.popularity) {
+          this.correctAnswerClicked();
+        } else {
+          alert(
+            `Game ended with a score of: ${this.score}\n${this.hiddenMovie.original_TITLE} had vote average of: ${this.hiddenMovie.popularity}`
+          );
+        }
+        break;
+      case 'runtime':
+        if (this.revealedMovie.runtime > this.hiddenMovie.runtime) {
+          this.correctAnswerClicked();
+        } else {
+          alert(
+            `Game ended with a score of: ${this.score}\n${this.hiddenMovie.original_TITLE} had vote average of: ${this.hiddenMovie.runtime}`
+          );
+        }
+        break;
+      case 'revenue':
+        if (this.revealedMovie.revenue > this.hiddenMovie.revenue) {
+          this.correctAnswerClicked();
+        } else {
+          alert(
+            `Game ended with a score of: ${this.score}\n${this.hiddenMovie.original_TITLE} had vote average of: ${this.hiddenMovie.revenue}`
+          );
+        }
+        break;
+    }
+  }
+
+  public correctAnswerClicked(): void {
+    this.score++;
+    this.revealedMovie = this.hiddenMovie;
+    this.getMovie(Math.round(Math.random() * this.totalMoviesCount));
+  }
 }
